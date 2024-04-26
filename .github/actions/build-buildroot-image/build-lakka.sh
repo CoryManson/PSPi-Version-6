cdd72694723c1a91d0ac0815d197779cc0ec1d18
Cory Manson <5052021+CoryManson@users.noreply.github.com>
2024-04-26 15:19:10 +1000

    change how we reference version

--- .github/actions/build-buildroot-image/build-lakka.sh
+++ .github/actions/build-buildroot-image/build-lakka.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set +x
+
+# "Permission denied" in the github workflow means that your script file does not have the "execute" permission set.
+# git update-index --chmod=+x .\.github\actions\build-buildroot-image\build-lakka.sh
 
 mkdir -p completed_images
 cd images
