fce6d0a5ea6ad21dc19832e981eb28ca5cf6da54
Cory Manson <5052021+CoryManson@users.noreply.github.com>
2024-04-26 13:01:03 +1000

    break buildroot images out into own scripts

---
+++ .github/actions/build-buildroot-image/build-lakka.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+mkdir -p completed_images
+cd images
+
+# Create mount directory and mount image file
+echo "Make mount directory and mount image"
+devicePath=$(sudo losetup -f)
+sudo losetup -Pf $IMAGE_NAME
+sudo losetup -l
+sudo mkdir /mnt/rootfs
+sudo mount -o loop,offset=$((512*8192)) $devicePath /mnt/rootfs
+
+# Add files to /boot
+echo "Add files to /boot"
+sudo cp $GITHUB_WORKSPACE/rpi/configs/lakka/config.txt /mnt/rootfs/config.txt
+sudo cp $GITHUB_WORKSPACE/rpi/configs/lakka/distroconfig.txt /mnt/rootfs/distroconfig.txt
+sudo cp $GITHUB_WORKSPACE/rpi/overlays/* /mnt/rootfs/overlays/
+sudo mkdir -p /mnt/rootfs/drivers
+sudo cp $GITHUB_WORKSPACE/rpi/drivers/bin/* /mnt/rootfs/drivers/
+
+# Copy squashfs to working directory
+echo "Copy squashfs to working directory"
+cp /mnt/rootfs/SYSTEM ./SYSTEM
+
+# Unpack SYSTEM squashfs
+echo "Unpack SYSTEM squashfs"
+sudo unsquashfs SYSTEM
+
+# Add autostart.sh
+echo "Add autostart.sh"
+sudo cp $GITHUB_WORKSPACE/rpi/scripts/lakka/autostart.sh ./squashfs-root/usr/config/autostart.sh
+sudo chmod +x ./squashfs-root/usr/config/autostart.sh
+
+# Add joypad autoconfig
+echo "Add joypad autoconfig"
+sudo cp $GITHUB_WORKSPACE/rpi/configs/lakka/PSPi-Controller.cfg ./squashfs-root/etc/retroarch-joypad-autoconfig/udev/PSPi-Controller.cfg
+
+# Repack squashfs
+echo "Repack squashfs"
+sudo mksquashfs squashfs-root filesystem.squashfs -comp xz
+
+# Copy squashfs back to image
+echo "Copy squashfs back to image"
+sudo cp filesystem.squashfs /mnt/rootfs/SYSTEM
+
+# Unmount image
+echo "Unmount image"
+sudo umount /mnt/rootfs
+sudo losetup -d $devicePath
+
+# Recompress image
+echo "Recompress image"
+xz -z "$IMAGE_NAME"
+
+# Move image to completed_images and rename
+echo "Move image to completed_images and rename"
+mv "$IMAGE_NAME.xz" ../completed_images/$PSPI_IMAGE_NAME
---
+++ .github/actions/build-buildroot-image/build-recalbox.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+mkdir -p completed_images
+cd images
+
+# Create mount directory and mount image file
+echo "Make mount directory and mount image"
+sudo mkdir -p /mnt/rootfs
+sudo mount -o loop,offset=$((512*2048)) "$IMAGE_NAME" /mnt/rootfs
+
+# Add files to /boot
+echo "Add files to /boot"
+sudo cp $GITHUB_WORKSPACE/rpi/configs/recalbox/config.txt /mnt/rootfs/config.txt
+sudo cp $GITHUB_WORKSPACE/rpi/configs/recalbox/recalbox-user-config.txt /mnt/rootfs/recalbox-user-config.txt
+sudo cp $GITHUB_WORKSPACE/rpi/overlays/* /mnt/rootfs/overlays/
+sudo mkdir -p /mnt/rootfs/drivers
+sudo cp $GITHUB_WORKSPACE/rpi/drivers/bin/* /mnt/rootfs/drivers/
+
+# Copy squashfs to working directory
+echo "Copy squashfs to working directory"
+sudo cp /mnt/rootfs/boot/recalbox ./recalbox
+
+# Unpack squashfs
+echo "Unpack squashfs"
+unsquashfs recalbox
+
+# Add custom.sh to recalbox
+echo "Add custom.sh to recalbox"
+cp $GITHUB_WORKSPACE/rpi/scripts/recalbox/custom.sh ./squashfs-root/recalbox/share_init/system/custom.sh
+chmod +x ./squashfs-root/recalbox/share_init/system/custom.sh
+
+# Update S12populateshare to copy custom.sh into system at boot
+echo "Update S12populateshare to copy custom.sh into system at boot"
+sed -i "`wc -l < ./squashfs-root/etc/init.d/S12populateshare`i\\# copy pspi custom.sh\\" ./squashfs-root/etc/init.d/S12populateshare
+sed -i "`wc -l < ./squashfs-root/etc/init.d/S12populateshare`i\\cp "/recalbox/share_init/system/custom.sh" "/recalbox/share/system/custom.sh"\\" ./squashfs-root/etc/init.d/S12populateshare
+
+# Add driver libraries
+echo "Add driver libraries"
+cp $GITHUB_WORKSPACE/rpi/libraries/recalbox/* ./squashfs-root/usr/lib/
+
+# repack squashfs
+echo "Repack squashfs"
+mksquashfs squashfs-root filesystem.squashfs -comp xz
+
+# Copy squashfs back to image
+echo "Copy squashfs back to image"
+sudo cp filesystem.squashfs /mnt/rootfs/boot/recalbox
+
+# Unmount image
+echo "Unmount image"
+sudo umount /mnt/rootfs
+
+# Recompress image
+echo "Recompress image"
+xz -z "$IMAGE_NAME"
+echo "Move image to completed_images & rename"
+mv "$IMAGE_NAME.xz" ../completed_images/$PSPI_IMAGE_NAME
