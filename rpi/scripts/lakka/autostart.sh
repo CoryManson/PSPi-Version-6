a942c24c3caeba5954015e07692ee6aee1ff2e74
Cory Manson <5052021+CoryManson@users.noreply.github.com>
2024-05-21 22:55:34 +1000

    Allow drivers to be configured from boot partition

--- rpi/scripts/lakka/autostart.sh
+++ rpi/scripts/lakka/autostart.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+. /flash/pspi.conf
+
+echo "enable_dim: $enable_dim"
+echo "dim_seconds: $dim_seconds"
+echo "fast_mode: $fast_mode"
+echo "disable_crc: $disable_crc"
+echo "disable_gamepad: $disable_gamepad"
+echo "joysticks: $joysticks"
+
+params=""
+
+# Function to detect the architecture of the operating system
 detect_architecture() {
     local arch
     arch=$(uname -m)
@@ -14,9 +26,27 @@ detect_architecture() {
             ;;
     esac
 }
-
 detect_architecture
 
-/flash/drivers/main$ARCH_SUFFIX --dim &
+# Set additional parameters based on configuration variables
+if [ "$enable_dim" = "true" ]; then
+    params="$params --dim $dim_seconds  "
+fi
+
+if [ "$fast_mode" = "true" ]; then
+    params="$params --fast "
+fi
+
+if [ "$disable_crc" = "true" ]; then
+    params="$params --nocrc "
+fi
+
+if [ "$disable_gamepad" = "true" ]; then
+    params="$params --nogamepad "
+fi
+
+echo "Starting PSPi with parameters: $params"
+
+/flash/drivers/main$ARCH_SUFFIX $params &
 sleep 1
 /flash/drivers/osd$ARCH_SUFFIX &
