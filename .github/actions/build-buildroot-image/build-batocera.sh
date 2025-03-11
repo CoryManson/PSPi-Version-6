cb7393ac4ede92bdbd4bb5fadac3bd7d5fe6b011
Cory Manson <5052021+CoryManson@users.noreply.github.com>
2025-03-11 16:14:48 +1100

    Split large images into parts

--- .github/actions/build-buildroot-image/build-batocera.sh
+++ .github/actions/build-buildroot-image/build-batocera.sh
@@ -84,3 +84,12 @@ echo "Compress image"
 gzip -9 $IMAGE_NAME
 echo "Move image to completed_images & rename"
 mv $IMAGE_NAME.gz ../completed_images/$PSPI_IMAGE_NAME
+
+# Split image to multiple parts if over 1.9GB
+FILE_SIZE=$(stat -c%s "../completed_images/$PSPI_IMAGE_NAME")
+if [ $FILE_SIZE -gt $((1900*1024*1024)) ]; then
+  echo "Image is larger than 1.9GB. Splitting..."
+  cd ../completed_images
+  split -b 1900M "$PSPI_IMAGE_NAME" "$PSPI_IMAGE_NAME.part"
+  rm "$PSPI_IMAGE_NAME"
+fi
